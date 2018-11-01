c872d13cd8504249a058f4394cb5d21c7eb4b406
shrike94 <32710760+shrike94@users.noreply.github.com>
2018-11-01 10:07:42 -0500

    Add files via upload

---
+++ InstallFortuneCow.ps1
@@ -0,0 +1,21 @@
+﻿Configuration InstallFortuneCow
+{
+    Import-DSCResource -Module nx 
+
+    Node "localhost"
+    {
+        nxPackage fortune
+        {
+            Name = "fortune"
+            Ensure = "Present"
+            PackageManager = "apt"
+        }
+
+        nxPackage cowsay
+        {
+            Name = "cowsay"
+            Ensure = "Present"
+            PackageManager = "apt"
+        }
+    }
+}
